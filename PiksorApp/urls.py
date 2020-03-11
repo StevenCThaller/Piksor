e10b85bdff918f358138acab54680166cf281e1e
StevenCThaller <stevencthaller@gmail.com>
2020-03-10 17:01:30 -0700

    Added some Creating operation functionality

--- PiksorApp/urls.py
+++ PiksorApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('series/<int:series_id>', views.one_series)
+    path('series/<int:series_id>', views.one_series),
+    path('addcharacter', views.addcharacter),
+    path('addseries', views.addseries)
 ]
--- PiksorApp/views.py
+++ PiksorApp/views.py
@@ -5,7 +5,8 @@ from .models import PixarCharacter, Series
 
 def index(request):
     context = {
-        "characters": PixarCharacter.objects.all()
+        "characters": PixarCharacter.objects.all(),
+        "all_series": Series.objects.all()
     }
     return render(request, "index.html", context)
 
@@ -15,3 +16,37 @@ def one_series(request, series_id):
         "this_series": Series.objects.get(id=series_id)
     }
     return render(request, "series.html", context)
+
+
+def addcharacter(request):
+    # Get the character name from the form
+    char_name = request.POST['name']
+    # Get the character height form the form
+    char_height = request.POST['height']
+    # Get the character description from the form
+    char_description = request.POST['description']
+    # Get the series with the id that's in the form
+    char_series = Series.objects.get(id=request.POST['series'])
+
+    # Now let's take these things and actually CREATE this object.
+    PixarCharacter.objects.create(
+        name=char_name, height=char_height, description=char_description, series=char_series)
+
+    # Awesome! we created the character. now let's redirect back to the main page!
+    return redirect('/')
+
+
+def addseries(request):
+    # Get the series name from the form
+    series_name = request.POST['series_name']
+    # Get the series release date from the form
+    series_date = request.POST['first_release_date']
+    # Get the series description from the form
+    series_description = request.POST['description']
+
+    # Now let's take these things and actually CREATE this object!
+    Series.objects.create(series_name=series_name,
+                          first_release_date=series_date, description=series_description)
+
+    # Sweet! Now let's go back to the main page!
+    return redirect('/')
